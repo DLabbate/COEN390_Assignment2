f5a17694970131032ecea38beccc74adc5df6edc
Domenic Labbate <domeniclabbate@gmail.com>
2020-02-12 00:22:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/coen390_assignment2/Course.java
@@ -0,0 +1,22 @@
+package com.example.coen390_assignment2;
+
+public class Course {
+    protected String courseTitle;
+    protected String courseCode;
+
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+}
